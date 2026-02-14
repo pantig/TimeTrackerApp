60a71e9311693ef5733c22eab806ce9840e6d028
Oliwer Księżycki <61838534+pantig@users.noreply.github.com>
2026-02-14 20:40:00 +0100

    Add missing using System.ComponentModel.DataAnnotations in EmployeesController

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using System.Security.Claims;
 using TimeTrackerApp.Data;
 using TimeTrackerApp.Models;
